ec63f221450fecc7dc3fb1dcf75e5f2968a14f47
mkrolczyk12 <m.krolczyk66@gmail.com>
2023-04-15 19:36:09 +0200

    mkrolczyk - set up docker-compose, added liquibase for db management and prepared demo version of local infrastructure

--- infrastructure/README.md
+++ infrastructure/README.md
@@ -1,1 +1,14 @@
-## TODO -> add README.md
+# Infrastructure
+
+This folder contains the infrastructure for the project, including local infrastructure built using Docker.
+
+## Description
+
+In this folder, you will find code and configuration files that allow you to create and manage the project infrastructure. </br>
+
+Available environments
+1. Local
+
+## Requirements
+
+To use the infrastructure in this folder, you must have Docker installed, as well as other required project dependencies. Details can be found in the configuration files and instructions in the individual subfolders.
---
+++ services/db/liquibase.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# This script uses Liquibase to manage database migrations. The script executes Liquibase commands inside a Docker container.
+#
+# List of liquibase available commands:
+# https://docs.liquibase.com/commands/home.html
+#
+# The available parameters are:
+#
+#    - COMMAND: the Liquibase command to be executed. This is the only required parameter, and should be provided as an argument in the command line.
+#    - USERNAME: Parameter to define the database user credentials.
+#    - PASSWORD: Parameter to define the database user password credentials.
+#    - NETWORK_NAME: Parameter to define the name of the Docker network that the container should be attached to.
+#    - DB_URL: Parameter to define the URL for the database connection.
+#
+# If the user fails to provide the COMMAND parameter when running the script, an error message is displayed, indicating the correct usage of the script.
+#
+# Otherwise, the script runs the Liquibase container in the network defined by the %NETWORK_NAME% parameter, mounting the changelog directory as the /liquibase/changelog/ directory in the container.
+#
+# With this script, you can easily manage your database migrations using Liquibase.
+LIQUIBASE_VERSION=4.19
+
+COMMAND=$1
+USERNAME=$2
+PASSWORD=$3
+NETWORK_NAME=$4
+DB_URL=$5
+
+#USERNAME=postgres
+#PASSWORD=postgres
+#NETWORK_NAME=local_book_shop
+#DB_URL=jdbc:postgresql://bookshop_db:5432/bookshop_db
+
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ]; then
+  echo echo "Error: missing argument. Usage: $0 [command] [username] [password] [network_name] [db_url]"
+else
+docker run \
+  --network $NETWORK_NAME \
+  --rm \
+  -v "$PWD"/changelog:/liquibase/changelog/ \
+liquibase/liquibase:$LIQUIBASE_VERSION "$COMMAND" \
+  --changelog-file=db.changelog.xml \
+  --url=$DB_URL \
+  --username=$USERNAME \
+  --password=$PASSWORD \
+  --driver=org.postgresql.Driver
+fi
