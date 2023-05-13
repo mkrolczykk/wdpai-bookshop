7490f4fa8565a808b5cdfd8e136b30d360f555bd
mkrolczykk <m.krolczyk66@gmail.com>
2023-05-13 15:29:09 +0200

    mkrolczyk - js; added functions for removing items from favorites list and shopping cart

---
+++ services/bookshop/public/js/favorites/remove-from-favorites.js
@@ -0,0 +1,41 @@
+
+const removeFromFavoritesEndpoint = 'http://localhost:8180/api/v1/favorites/remove-from-favorites.php';
+
+function removeFromFavorites(bookId) {
+
+    const requestBody = {
+        bookId: bookId
+    };
+
+    fetch(removeFromFavoritesEndpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Request failed with status ' + response.status);
+            }
+        })
+        .then(data => {
+            if (data && data.status === 200) {
+                // Success - book has been removed favorites
+                alert(data.message);
+            } else if (data && data.status === 400) {
+                // Fail - book not present in favorites
+                alert(data.message);
+            } else {
+                // Internal server errors
+                alert('Server error: ' + data.message);
+            }
+        })
+        .catch(error => {
+            // HTTP exceptions
+            alert('Request failed: ' + error.message);
+            console.log(error);
+        });
+}
