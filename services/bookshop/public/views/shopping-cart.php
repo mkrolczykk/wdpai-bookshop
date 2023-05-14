1132adead57a0220919b80fcf198206d8949b691
mkrolczykk <m.krolczyk66@gmail.com>
2023-05-14 15:23:41 +0200

    mkrolczyk - improved shopping cart feature

--- services/bookshop/public/views/shopping-cart.php
+++ services/bookshop/public/views/shopping-cart.php
@@ -26,8 +26,8 @@
         <?php if (!empty($shoppingCartResult)): ?>
             <table class="shopping-cart-table">
                 <thead>
-                <tr>
-                    <th>Book name</th>
+                <tr class="cart-header">
+                    <th>Book</th>
                     <th>Price</th>
                     <th>Amount</th>
                     <th>For all</th>
@@ -36,42 +36,47 @@
                 </tr>
                 </thead>
                 <tbody>
-                    <?php foreach ($shoppingCartResult as $item): ?>
-                        <tr class="cart-row">
-                            <td>
-                                <a href="bookDetail?bookTitle=<?php echo str_replace(' ', '-', $item->getBookName()); ?>">
-                                    <?php echo $item->getBookName(); ?>
-                                </a>
-                            </td>
-                            <td class="price"><?php echo $item->getPrice(); ?></td>
-                            <td class="cart-button">
-                                <div class="amount-button">
-                                    <i class="minus fa fa-minus fa-sm" data-book-id="<?php echo $item->getBookId(); ?>"></i>
-                                    <span class="amount"><?php echo $item->getAmount(); ?></span>
-                                    <i class="plus fa fa-plus fa-sm" data-book-id="<?php echo $item->getBookId(); ?>"></i>
+                <?php foreach ($shoppingCartResult as $item): ?>
+                    <tr class="cart-row">
+                        <td>
+                            <div class="cart-row-book">
+                                <div class="cart-row-book-content">
+                                    <img src="public/img/books/<?php echo str_replace(' ', '-', strtolower($item->getBookName())); ?>.png" alt="Book Cover" onerror="this.src='public/img/books/mock-book-detail-cover.png'" class="book-cover">
+                                    <a href="bookDetail?bookTitle=<?php echo str_replace(' ', '-', $item->getBookName()); ?>">
+                                        <?php echo $item->getBookName(); ?>
+                                    </a>
                                 </div>
-                            </td>
-                            <td class="total"><?php echo $item->getTotal(); ?></td>
-                            <td id="currency"><?php echo $item->getCurrency(); ?></td>
-                            <td>
-                                <button class="remove-button" data-item-id="<?php echo $item->getBookName(); ?>">
-                                    <i class="fa fa-times fa-lg"></i>
-                                </button>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                    <tr class="cart-total-row">
-                        <td colspan="3"></td>
-                        <td class="cart-shipping" colspan="2">
-                            Shipping cost: <span id="shippingCost">Free</span></td>
-                        <td></td>
+                            </div>
+                        </td>
+                        <td class="price"><?php echo $item->getPrice(); ?></td>
+                        <td class="cart-button">
+                            <div class="amount-button">
+                                <i class="minus fa fa-minus fa-sm" data-book-id="<?php echo $item->getBookId(); ?>"></i>
+                                <span class="amount"><?php echo $item->getAmount(); ?></span>
+                                <i class="plus fa fa-plus fa-sm" data-book-id="<?php echo $item->getBookId(); ?>"></i>
+                            </div>
+                        </td>
+                        <td class="total"><?php echo $item->getTotal(); ?></td>
+                        <td id="currency"><?php echo $item->getCurrency(); ?></td>
+                        <td>
+                            <button class="remove-button" data-item-id="<?php echo $item->getBookName(); ?>">
+                                <i class="fa fa-times fa-lg"></i>
+                            </button>
+                        </td>
                     </tr>
-                    <tr class="cart-total-row">
-                        <td colspan="3"></td>
-                        <td class="cart-total" colspan="2">
-                            Total: <span id="cartTotalAmount"><?php echo "{$totalAmount}"?></span><span id="cartTotalCurrency"><?php echo " {$shoppingCartResult[0]->getCurrency()}"; ?></span></td>
-                        <td></td>
-                    </tr>
+                <?php endforeach; ?>
+                <tr id="cart-total-start" class="cart-total-row">
+                    <td colspan="3"></td>
+                    <td class="cart-shipping" colspan="2">
+                        Shipping cost: <span id="shippingCost">Free</span></td>
+                    <td></td>
+                </tr>
+                <tr class="cart-total-row">
+                    <td colspan="3"></td>
+                    <td class="cart-total" colspan="2">
+                        Total: <span id="cartTotalAmount"><?php echo "{$totalAmount}"?></span><span id="cartTotalCurrency"><?php echo " {$shoppingCartResult[0]->getCurrency()}"; ?></span></td>
+                    <td></td>
+                </tr>
                 </tbody>
             </table>
             <div class="shopping-cart-button button">
